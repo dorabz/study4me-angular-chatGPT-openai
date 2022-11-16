3aa10c0a48d3c960c2166e3f997890b4041639ff
Lonky131 <kl50222@fer.hr>
2022-11-16 18:38:21 +0100

    Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ResultComponent } from './components/result/result.component';
+import { UploadComponent } from './components/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    pathMatch : 'full',
+    redirectTo : 'upload'
+  },
+  {
+    path : 'upload',
+    pathMatch : 'full',
+    component : UploadComponent
+  },
+  {
+    path : 'result',
+    pathMatch : 'full',
+    component : ResultComponent
+  },
+  {
+    path : '**',
+    component : NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './components/result/result.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    ResultComponent,
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
